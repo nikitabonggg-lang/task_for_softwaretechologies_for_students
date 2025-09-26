fe835326dd180cee5471560af016c319e445ffb8
naukolov <nikitabonggg@gmail.com>
2025-09-26 09:58:42 +0300

    reverse

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -10,7 +10,19 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
+        if (array == null){
+            return;
+        }
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right){
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right++;
+        }
     }
 
     /**
